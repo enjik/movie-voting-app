1727a013c9ebd1a19e33edc8cf34d73a5a2fdd03
Enji <enjikim17@gmail.com>
2018-11-30 17:19:22 -0800

    Create RESTful API skeleton

--- server/index.js
+++ server/index.js
@@ -7,9 +7,25 @@ const app = express();
 // Port to listen to
 const PORT = 3000;
 
+// Get all movies by desc number of movie votes
+app.get('/api/movies', (req, res) => {
 
+});
 
+// Add new movie to list
+app.post('/api/movies', (req, res) => {
 
+});
+
+// Upvote a movie
+app.post('/api/movies/:movieId/like', (req, res) => {
+
+});
+
+// Downvote a movie
+app.post('/api/movies/:movieId/dislike', (req, res) => {
+
+});
 
 
 
