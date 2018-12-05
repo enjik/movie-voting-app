39737b1ce23c130e3d17c41253f681c782c1eef7
Enji <enjikim17@gmail.com>
2018-12-05 01:05:58 -0800

    Insert sample movie data into database

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,14 @@ CREATE TABLE movies (
   down_votes int NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO movies (title, total_votes, up_votes, down_votes)
+VALUES
+  ("A Star is Born", 47, 40, 7),
+  ("Avengers: Infinity War", 140, 120, 20),
+  ("Ralph Breaks the Internet", 98, 83, 15),
+  ("Black Panther", 135, 130, 5),
+  ("Mary Poppins Returns", 24, 24, 0),
+  ("Incredible 2", 87, 83, 4),
+  ("Searching", 54, 44, 10),
+  ("Crazy Rich Asians", 105, 80, 25);
